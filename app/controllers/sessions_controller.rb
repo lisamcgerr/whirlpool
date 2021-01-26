3eaa1f4eaaf84229e5cd8a8458869d83edcc726a
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-26 12:23:21 -0600

    worked on create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,25 @@ class SessionsController < ApplicationController
     end
 
     def create
-        #byebug
-        date = Session
+        byebug
         @session = Session.new(session_params)
+        byebug
         
     end
 
+
     private
     def session_params
+        params["session"]["date"] = generate_date(params) 
         params.require(:session).permit(:date, :game_id, :public)
+    end
+
+    def generate_date(params)
+        date = DateTime.new(params["session"]["date(1i)"].to_i,
+        params["session"]["date(2i)"].to_i,
+        params["session"]["date(3i)"].to_i,
+        params["session"]["date(4i)"].to_i,
+        params["session"]["date(5i)"].to_i
+      )
     end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -3,4 +3,5 @@ class Session < ApplicationRecord
   has_many :user_sessions
   has_many :users, through: :user_sessions
 
+  
 end
