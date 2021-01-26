973d895dc3df49a2ef26df9deb7fa705f6a4ba58
Marc <marc.ferraro1@gmail.com>
2021-01-25 18:12:35 -0600

    Community Index

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,2 +1,11 @@
 class CommunitiesController < ApplicationController
+
+    def index
+        @communities = Community.all
+    end
+
+    def show
+        @community = Community.find_by(id: params[:id])
+    end
+
 end
