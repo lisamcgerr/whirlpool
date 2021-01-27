a6ba81242144389b320a0b2bd5ae4b83f1568297
Marc <marc.ferraro1@gmail.com>
2021-01-27 15:53:03 -0600

    Session Editing

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   get "/login", to: "logins#new"
   post "/login", to: "logins#create"
   delete "/logout", to: "logins#logout"
+
+  get "/session/remove_user", to: "sessions#remove_user", as: "remove_user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
