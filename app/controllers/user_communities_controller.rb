79664921c31b92c4e65845a8c566a91354be575f
Marc <marc.ferraro1@gmail.com>
2021-01-27 23:24:20 -0600

    Join Community Button

--- app/controllers/user_communities_controller.rb
+++ app/controllers/user_communities_controller.rb
@@ -1,2 +1,16 @@
 class UserCommunitiesController < ApplicationController
+
+    def new
+    end
+
+    def create
+        
+    end
+    
+    private
+
+    def user_community_params
+        params.require
+    end
+
 end
